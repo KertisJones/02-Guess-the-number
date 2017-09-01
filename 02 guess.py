842d4341f3e6ed6e0bc73d9d97d52f711017f993
Jesse Riggins <jdriggin@umail.iu.edu>
2017-09-01 01:21:11 -0400

    Completed assignment 2. Updated license and readme.

--- 02 guess.py
+++ 02 guess.py
@@ -1,31 +1,73 @@
-#!/Library/Frameworks/Python.framework/Versions/3.6/bin
+#!/usr/bin/env python3
 
 import sys
 import random
+
 assert sys.version_info >= (3,4), "This script requires at least Python 3.4"
 
-
-#generate a random integer between 1 and 20 (inclusive) and store it in the variable [number]
-number = random.randint(1,20)
-
-
-
-#ask the user for a response and store it in the variable [guess]
-guess = input()
+ranNum = random.randint(0, 100)
+guessNum = -1
+guessRemaining = 5
+stillPlaying = True
 
 
+def RepresentsInt(s):
+    try: 
+        int(s)
+        return True
+    except ValueError:
+        return False
 
-#a try/except block is a great tool for programmers to be able to deal with errors. In this instance, it reports an error if the user enters something other than an integer
-try:
-	#convert the guess to an integer
-	guess = int(guess)
-
-
-	#check if the guess is less than the random number
-	if guess < number:
-		print('Too low!')
-
-
-except ValueError:
-	print('Please enter a whole number.')
-
+    
+while (stillPlaying):
+    print("Guess the number that I'm thinking of from 0 to 100. You have 5 tries.")
+    print("")
+    while (guessRemaining > 0):
+        guessNum = input("~ ")
+        if (RepresentsInt(guessNum)):
+            guessNum = int(guessNum)
+            if(guessNum == ranNum):
+                print("You Got it! " + str(guessNum) + " is the answer!")
+                print("")
+                guessRemaining = 0
+            else:
+                if(guessNum == 69):            
+                    print("Seriously?")
+                if(guessNum > ranNum):            
+                    print("Not quite, " + str(guessNum) + " is too high.")
+                elif(guessNum < ranNum):
+                    print("Not quite, " + str(guessNum) + " is too low.")
+                else:
+                    print("You're not supposed to see this...")
+                    print("")
+                    guessRemaining = 0
+                guessRemaining = guessRemaining - 1
+                if (guessRemaining != 1):
+                    print("You have " + str(guessRemaining) + " tries remaining.")
+                else:
+                    print("You have " + str(guessRemaining) + " try remaining.")
+                print("")
+        else:
+            print("Oops! " + guessNum + " is not a number! Try again.")
+    if(guessNum == ranNum):
+        print("YOU WIN!")
+        print("")
+        print("The number was " + str(ranNum) + "!")
+        guessRemaining = 0
+    else:
+        print("GAME OVER")
+        print("")
+        print("The number was " + str(ranNum) + ".")
+    if(ranNum == 69):            
+        print("lol")
+    print("")
+    print("--------------------------------------------------")
+    playAgain = input("PLAY AGAIN? (y/n) ").lower()
+    if("y" in playAgain):
+        ranNum = random.randint(0, 100)
+        guessRemaining = 5
+        stillPlaying = True
+    else:
+        stillPlaying = False
+    print("--------------------------------------------------")
+    print("")
--- README.md
+++ README.md
@@ -1,22 +1,12 @@
 # 02-Guess-the-number
 
-Guess the number! This is the second programming assignment for ILS-Z399. The requirements for this assignment are as follows:
+Guess the number! This is the second programming assignment for ILS-Z399.
 
-* Write a Python program for playing a simple "guess the number" game
-* The program should randomly choose a number and then invite the user to guess the chosen number. 
-* A game is only fun if it holds out a possibility for mastery, so the user will need to be given multiple chances to guess.
-* Try to give the player useful feedback; you can decide how many chances the player will get.
-
-To get a perfect score, you will need to think creatively! Think about ways you can make this project more interesting or fun.
-
-To complete this assignment you will need to understand:
-
-* Python [integers and strings](https://docs.python.org/3.7/library/stdtypes.html)
-* How to [convert a string into an integer](https://stackoverflow.com/questions/379906/parse-string-to-float-or-int)
-* How to generate a [random integer](https://docs.python.org/3.6/library/random.html)
-* How to use basic control structures like [if and else (and elif)](http://anh.cs.luc.edu/python/hands-on/3.1/handsonHtml/ifstatements.html)
-* How to [compare](https://www.tutorialspoint.com/python/comparison_operators_example.htm) two numbers in Python
-* How to [accept user input](http://anh.cs.luc.edu/python/hands-on/3.1/handsonHtml/io.html)
+* This is a simple "guess the number" game
+* The program randomly chooses a number from 0 to 100
+* The player has 5 tries to guess it
+* Each time the player guesses, they are told if they guessed too high or low
+* The player is given the option to play again if they guess correctly or run out of tries
 
 ---
 
